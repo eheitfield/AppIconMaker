f32416eb692a293235d115b42ab8b54605d0d6e1
Erik Heitfield <eheitfield@gmail.com>
2020-02-29 11:02:32 -0500

    Initial Commit

---
+++ AppIconMaker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AppIconMaker
+//
+//  Created by Erik Heitfield on 2/29/20.
+//  Copyright © 2020 Erik Heitfield. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
